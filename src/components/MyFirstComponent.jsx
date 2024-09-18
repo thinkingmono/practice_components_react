5038b653be9e69a82ff8c49ae3916212362db727
Alejandro Garcia <thinkingmono92@gmail.com>
2024-09-17 20:08:15 -0500

    Sesión 17092024 - MyFirstComponent. React Fundamentals

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MyFirstComponent = () => {
+  return (
+    <>
+      <h2>Temas React</h2>
+      <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados</li>
+        <li>Hooks</li>
+        <li>Props</li>
+      </ul>
+    </>
+  )
+}
+
+export default MyFirstComponent
